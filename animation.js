98f4c815c51aa3a5699e8d4de4e2b3740825772d
karan bhai <dearsam.750@gmail.com>
2024-10-16 18:25:40 +0530

    added all files

---
+++ animation.js
@@ -0,0 +1,97 @@
+//----------------> Welcome text animation  <--------------------------
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 10000
+  });
+
+
+//----------------> typing animation  <-------------------------------
+ const texts = [
+  "Computer Fundamentals",
+  "Operating System",
+  "Discrete Mathematics",
+  "Business Law",
+  "MS-Excel",
+  "English",
+  "Hindi"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById('typing-text').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type, 200); // Adjust typing speed here (in milliseconds)
+}
+
+document.addEventListener('DOMContentLoaded', type);
+
+
+// Ai Useful link
+
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  // Class time table animation
+
+
+  var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: (el, i) => 45 * i
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
